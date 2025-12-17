2a5191d0ade4cff9caee6e932f81bd359419bf10
sachindhaygude16108-sketch <sachindhaygude16108@gmail.com>
2025-12-17 20:17:03 +0530

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,1 +1,20 @@
+function validateForm() {
 
+    if (document.getElementById("sname").value == "") {
+        alert("Please enter Student Name");
+        return false;
+    }
+
+    if (document.getElementById("mobile").value.length != 10) {
+        alert("Please enter valid 10-digit mobile number");
+        return false;
+    }
+
+    if (document.getElementById("class").value == "") {
+        alert("Please select class");
+        return false;
+    }
+
+    alert("Admission Form Submitted Successfully!");
+    return true;
+}
